37c09ce7a7564fd38e72bac1aa4b16bf17b516ed
jump <995536187@qq.com>
2019-06-21 15:24:51 +0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: 'postcss-scss',
+    plugins: [
+        require('precss'),
+        // require('cssnano')({
+        //     preset: 'default',
+        // }),
+    ]
+}
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>
+      Here is the demo of vue-fullcalendar, no jquery fullcalendar.js required!
+    </p>
+    <full-calendar class="test-fc" :events="fcEvents"
+                   first-day='1' locale="zh-cn"
+                   @changeMonth="changeMonth"
+                   @eventClick="eventClick"
+                   @dayClick="dayClick"
+                   @moreClick="moreClick">
+      <template slot="fc-event-card" scope="p">
+        <p><i class="fa"></i> {{ p.event.title }} test</p>
+      </template>
+    </full-calendar>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import FullCalendar from './fullCalendar'
+  let demoEvents = [
+    {
+      title    : 'Sunny 725-727',
+      start    : '2019-02-25',
+      end      : '2019-02-27',
+      cssClass : 'family'
+    },
+    {
+      title : 'Lunfel 726-727',
+      start : '2019-02-26',
+      end : '2019-02-27',
+      cssClass : ['home', 'work']
+    },
+    {
+      title : 'Lunfel 2/27-2/28',
+      start : '2019-02-27',
+      end : '2019-02-28'
+    },
+    {
+      title : 'Lunfel 2/27-2/28',
+      start : '2019-02-27',
+      end : '2019-02-28'
+    },
+    {
+      title : 'Lunfel 2/27-2/28',
+      start : '2019-02-27',
+      end : '2019-02-28'
+    },
+    {
+      title : 'Lunfel 2/26-3/05',
+      start : '2019-02-26',
+      end : '2019-03-05'
+    },
+    {
+      title : 'Lunfel 1/27-1/28',
+      start : '2019-01-27',
+      end : '2019-01-28'
+    },
+    {
+      title : 'Lunfel 1/27-2/2',
+      start : '2019-01-27',
+      end : '2019-02-02'
+    },
+    {
+      title : 'Lunfel 3/27-3/28',
+      start : '2019-03-27',
+      end : '2019-03-28'
+    },
+  ];
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data () {
+      return {
+        name:'Sunny!',
+        fcEvents : demoEvents
+      }
+    },
+    methods : {
+      'changeMonth' (start, end, current) {
+        console.log('changeMonth', start.format(), end.format(), current.format())
+      },
+      'eventClick' (event, jsEvent, pos) {
+        console.log('eventClick', event, jsEvent, pos)
+      },
+      'dayClick' (day, jsEvent) {
+        console.log('dayClick', day, jsEvent)
+      },
+      'moreClick' (day, events, jsEvent) {
+        console.log('moreCLick', day, events, jsEvent)
+      }
+    },
+    components : {
+      FullCalendar: FullCalendar
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .app{
+    color:green;
+  }
 </style>
